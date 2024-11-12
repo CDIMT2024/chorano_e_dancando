98e0de0773ce19e12659c9f37d38bd3019f378d1
EnricoGia <rico500000@gmail.com>
2024-11-12 18:23:59 -0300

    deps: maybe fix utils

--- spotfy-AI/utils.py
+++ spotfy-AI/utils.py
@@ -1,7 +1,11 @@
-from functools import cache
+import requests
 import joblib
-
+from io import BytesIO
+from functools import cache
 
 @cache
 def get_rf():
-    return joblib.load("./my_random_forest.joblib")
+    url = "https://github.com/CDIMT2024/chorano_e_dancando/raw/refs/heads/main/spotfy-AI/my_random_forest.joblib"
+    response = requests.get(url)
+    response.raise_for_status()  # Ensure we notice bad responses
+    return joblib.load(BytesIO(response.content))
